382a7b3648d30a859e3f59a850521d0ef579c957
charlesamacon <charles.a.macon@gmail.com>
2016-03-19 01:37:44 -0500

    Finished FIFO?
    Processes come in, processes go out in that order. We time everything,
    and call it a day.

--- fifo.cpp
+++ fifo.cpp
@@ -1,3 +1,4 @@
+// FIFO - Charles Alan Macon
 #include "gen_proc.h"
 
 using namespace std;
@@ -5,6 +6,8 @@ using namespace std;
 int main()
 {
 	processes* proc = new processes();
+	int time = 0;
+	int currentProcess = 0;
 	
 	for (int i = 0; i < NUM_OF_PROC; i++)
 	{
@@ -12,5 +15,35 @@ int main()
 		cout << "ID: " << proc->procs[i].getID() << "\tcpu: " << proc->procs[i].get_cycles() << "\tmem: " << proc->procs[i].get_mem() << "\tarr: " << proc->procs[i].get_arr() << endl;
 	}
 	
+	while (currentProcess < NUM_OF_PROC)
+	{
+		// Get process start time, wait until start time
+		int currentProcessStartTime = proc->procs[currentProcess].get_arr();
+		cout << "Time: " << time << endl;
+		cout << "Current Process: " << currentProcess << endl;
+		if (currentProcessStartTime <= time)
+		{
+			cout << "Running process" << endl;
+			// Get # of cycles in current process
+			int currentProcessCycles = proc->procs[currentProcess].get_cycles();
+			cout << "Current Process Cycles: " << currentProcessCycles << endl;
+			// Simulate process
+			for (currentProcessCycles; currentProcessCycles > 0; currentProcessCycles--)
+			{
+				//cout << "Cycles Remaining: " << currentProcessCycles << endl;
+				time++;
+			}
+			
+			// Move to next process
+			currentProcess++;
+		}
+		else
+		{
+			cout << "Waiting to start process" << endl;
+			time++;
+		}
+	}
+	
+	cout << "Finished FIFO simulation at time: " << time << endl;
 	return 0;
 }
